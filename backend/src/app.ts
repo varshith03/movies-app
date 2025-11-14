6ed6819a80b4c95f9a76aa17d2207993117599db
royalpinto333 <royal-rithesh.pinto@nexthink.com>
2025-11-14 10:31:49 +0530

    Code fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,13 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
